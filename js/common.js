ac5e754076a13f5538b4f68d90dcc1da23de9ad2
Oksana <oksana_yuma@mail.ru>
2020-09-17 12:18:53 +0500

    add icons

--- js/common.js
+++ js/common.js
@@ -13,6 +13,20 @@ var mySwiper = new Swiper('.hero_slider', {
     nextEl: '.hero_slider_button-next',
     prevEl: '.hero_slider_button-prev',
   },
+})
 
+var mySwiper = new Swiper('.nav_slider', {
+  // Optional parameters
+  slidesPerView: 1,
+  loop :true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.nav_slider_button-next',
+    prevEl: '.nav_slider_button-prev',
+  },
 })
